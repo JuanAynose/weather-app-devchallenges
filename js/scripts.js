b7ae30574f18cb0c14b5778f39ab7380b14652e4
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-06-08 19:09:15 -0300

    index.html temp changed
    nashe

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,25 @@ const identify = {
 const date = new Date();
 const [month, day, daynumber] = [date.getMonth(), date.getDate(),date.getDay()];
 let monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+let iconNumbers = ['brokenclouds','clearsky','clouds','fewclouds','mist','rain','scatteredclouds','showerrain','snow','thunderstorm'];
 let weakNames= ['Mon','Tus','Wed','Thu','Fri','Sat','Sun'];
+
+console.log(containerControl.children[1].children[0].children)
+const setdays= () =>{
+    let dayCounter=0, dayReset= 0, count=0;
+    for(const daysFiller of containerControl.children[1].children[0].children){
+        if(daynumber+count>6){
+            count=0
+            daysFiller.children[0].textContent=`${weakNames[count]}, ${day+dayCounter} ${monthNames[month]}`;
+        }else{
+            daysFiller.children[0].textContent=`${weakNames[daynumber+count]}, ${day+count} ${monthNames[month]}`;
+            count+=1;
+        }
+        console.log(daysFiller.children[0])
+        dayCounter+=1;
+    }
+    containerControl.children[1].children[0].children[0].children[0].textContent='Tomorrow';
+}
 
 const setValues = (res)=>{
     console.log(containerControl.children[0].children[3].children[0])
@@ -22,18 +40,14 @@ const setValues = (res)=>{
     //let temp__unite__left = containerControl.children[0].children[2].children[1];
     let placeName = containerControl.children[0].children[3].children[1].children[1].children[1].textContent =res.name;
     let todayDate = containerControl.children[0].children[3].children[1].children[0].children[0].textContent=`${weakNames[daynumber]}, ${day} ${monthNames[month]}`;
-    //let image
     let textTimeView = res.weather[0].main.replace(/\s+/g, '').toLowerCase();
     let timeView = containerControl.children[0].children[3].children[0].textContent=res.weather[0].main;
     let imageTime = containerControl.children[0].children[1].children[0].src=`../assets/images/${textTimeView}.png`;
     console.log(textTimeView);
     console.log(res);
     root.style.setProperty('--var-lenght-humidity', res.main.humidity +'%' );
+    setdays()
 }
-
-const addExtraInformation = ( need) =>{
-    console.log(need)
-};
 
 const catchError = (err)=> console.log(err);
 
@@ -54,26 +68,15 @@ const request =(url)=>{
     fetch(url)
         .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
         .then(res => res.json())
-        .catch(catchError)
-}
-
-//`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&lang=es&appid=${key_id}`
-const coordinates = (coords) =>{
-    console.log(coords)
-    
-    fetch((`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKEY}`))
-        .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
-        .then(res => res.json())
-        .then(catchError)
+        .then(setValues)
         .catch(catchError)
 }
 
 const requestLocation = (data)=>{
     if(data===identify.GET){
         const success = (position)=>{
-            coordinates({lat:position.coords.latitude,lon:position.coords.longitude})
+            request(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKEY}`);
         }
-        //request(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKEY}`);
         const error=()=> console.log('Unable to retrieve your location');
         navigator.geolocation.getCurrentPosition(success,error);
     }else if(data.SEND===identify.SEND){
