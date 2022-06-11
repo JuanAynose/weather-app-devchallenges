bad7dde23606afa0cfc1a2b43afce017f963be74
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-06-11 01:59:39 -0300

    few bugs fixed
    bug on the future cast;  days names.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ const menuModal = document.getElementById("bg__modal");
 const openModal = document.getElementById("open__modal");
 const form = document.getElementById("form");
 const containerControl = document.getElementById("container");
+const unitConverter = document.getElementById("unit__converter");
 let root = document.documentElement;
 
 const identify = {
@@ -15,38 +16,39 @@ const date = new Date();
 const [month, day, daynumber] = [date.getMonth(), date.getDate(),date.getDay()];
 let monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 let iconNumbers = ['fewclouds','clearsky','clouds','fewclouds','mist','rain','scatteredclouds','showerrain','snow','thunderstorm'];
-let weakNames= ['Mon','Tus','Wed','Thu','Fri','Sat','Sun'];
+let weakNames= ['Sun','Mon','Tus','Wed','Thu','Fri','Sat'];
 
-console.log(containerControl.children[1].children[0].children)
 const setdays= () =>{
-    let dayCounter=0, count=0;
-    for(const daysFiller of containerControl.children[1].children[0].children){
-        if(daynumber+count>6){
-            count=0
-            daysFiller.children[0].textContent=`${weakNames[count]}, ${day+dayCounter} ${monthNames[month]}`;
+    let dayCounter=0, count=0, dayReset=0;
+    for(const daysFiller of containerControl.children[1].children[1].children){
+        count+=1;
+        dayCounter+=1;
+        if(dayCounter+daynumber>6){
+            daysFiller.children[0].textContent=`${weakNames[dayReset++]}, ${day+count} ${monthNames[month]}`;
         }else{
             daysFiller.children[0].textContent=`${weakNames[daynumber+count]}, ${day+count} ${monthNames[month]}`;
-            count+=1;
         }
         let iconsCards = daysFiller.children[1].children[0].src=`../assets/images/${iconNumbers[Math.floor(Math.random() * 6)]}.png`;
-        dayCounter+=1;
     }
-    containerControl.children[1].children[0].children[0].children[0].textContent='Tomorrow';
+    containerControl.children[1].children[1].children[0].children[0].textContent='Tomorrow';
 }
 
 const setValues = (res)=>{
-    console.log(containerControl.children[0].children[3].children[0])
-    //let temp__text__left = containerControl.children[0].children[2].children[0];
+    let temp__text__left = containerControl.children[0].children[2].children[0].textContent=res.main.temp.toString().slice(0,1);
     //let temp__unite__left = containerControl.children[0].children[2].children[1];
     let placeName = containerControl.children[0].children[3].children[1].children[1].children[1].textContent =res.name;
     let todayDate = containerControl.children[0].children[3].children[1].children[0].children[0].textContent=`${weakNames[daynumber]}, ${day} ${monthNames[month]}`;
     let textTimeView = res.weather[0].main.replace(/\s+/g, '').toLowerCase();
     let timeView = containerControl.children[0].children[3].children[0].textContent=res.weather[0].main;
     let imageTime = containerControl.children[0].children[1].children[0].src=`../assets/images/${textTimeView}.png`;
-    console.log(textTimeView);
-    console.log(res);
+    let windSpeed = containerControl.children[1].children[2].children[1].children[0].children[1].children[0].textContent=res.wind.speed;
+    let humidityLevels = containerControl.children[1].children[2].children[1].children[1].children[1].children[0].textContent=res.main.humidity; 
+    let milesConverter = Number(res.visibility)*0.00062137;
+    let visibility = containerControl.children[1].children[2].children[1].children[2].children[1].children[0].textContent=milesConverter.toString().slice(0,3);
+    
+    let airPressure = containerControl.children[1].children[2].children[1].children[3].children[1].children[0].textContent=res.main.pressure;
     root.style.setProperty('--var-lenght-humidity', res.main.humidity +'%' );
-    setdays()
+    setdays();
 }
 
 const catchError = (err)=> console.log(err);
@@ -58,6 +60,7 @@ form.addEventListener("submit",(e)=>{
         SEND:identify.SEND,
         place:locationPlace
     })
+    menuModal.children[0].classList.remove("active");
 });
 
 searchBttnCountry.addEventListener("click",()=>{
@@ -75,14 +78,41 @@ const request =(url)=>{
 const requestLocation = (data)=>{
     if(data===identify.GET){
         const success = (position)=>{
-            request(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKEY}`);
+            request(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKEY}&units=metric`);
         }
         const error=()=> console.log('Unable to retrieve your location');
         navigator.geolocation.getCurrentPosition(success,error);
     }else if(data.SEND===identify.SEND){
-        request(`https://api.openweathermap.org/data/2.5/weather?q=${data.place}&appid=${apiKEY}`);
+        request(`https://api.openweathermap.org/data/2.5/weather?q=${data.place}&appid=${apiKEY}&units=metric`);
     }
 }
 
 openModal.addEventListener("click",()=>menuModal.children[0].classList.add("active"));
 menuModal.addEventListener("click", (e)=>{if(e.target.classList=="button__close") menuModal.children[0].classList.remove("active")});
+
+const modalCountry = document.getElementById("modal__country");
+modalCountry.addEventListener("click",(e)=>{
+    if(e.target.classList[0]=='country__sugges'){
+        let locationPlace=e.target.children[0].textContent;
+        requestLocation({
+        SEND:identify.SEND,
+        place:locationPlace
+        });
+        menuModal.children[0].classList.remove("active");
+    } 
+})
+
+unitConverter.addEventListener("click",(e)=>{
+    if(e.target.textContent==="°C"){
+        e.target.classList.add("active");
+        e.target.nextElementSibling.classList.remove("active")
+    for(const daysFiller of containerControl.children[1].children[1].children){
+        console.log(daysFiller)
+    }
+        
+    }else{
+       
+        e.target.classList.add("active");
+        e.target.previousElementSibling.classList.remove("active")
+    }
+})
